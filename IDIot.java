64f12edb60a105bd37e6ff3f90c974c788aba1a7
Dereandor <43048555+Dereandor@users.noreply.github.com>
2018-09-25 13:53:42 +0200

    Add files via upload

---
+++ IDIot.java
@@ -0,0 +1,21 @@
+/**
+ * Dato:		25.09.2018
+ * Forfattere:	Torstein H. Sundfær, Cato Bakken, Torbjørn B. Lauvvik, Jonas B. Jacobsen, Eirik Hemstad
+ *
+ * Historie:
+ * ---------
+ *
+ * 25.09.2018
+ * Første revisjon
+**/
+
+class IDIot {
+	public static void main(String[]args) {
+		System.out.println("IDIot");
+		aaaaaaaaaaaaaa
+		System.out.println("La oss telle til 100 flasker med øl!");
+		for (int i = 0; i < 100; i++) {
+			System.out.println((i + 1) + " flasker med øl.");
+		}
+	}
+}
